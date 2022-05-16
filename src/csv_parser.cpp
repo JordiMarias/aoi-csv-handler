8bf07bfca98297c04e3a7ad0bc962aef352558cf
Jordi Marias I Parella <jordi.marias@i2cat.net>
2022-05-16 23:34:51 +0200

    More debug97

--- src/csv_parser.cpp
+++ src/csv_parser.cpp
@@ -103,9 +103,9 @@ void CSVParser::parse_sent_positioned(const std::string& file_sent,const std::st
                     if (sm.str(16) == "1"){
                         std::cout << "Checking for a sent one" << std::endl;
                         std::cout << "before" << std::endl;
-                        std::cout << sm_f.str(1) << std::endl;
+                        std::cout << sm.str(1) << std::endl;
                         std::cout << "After"<< std::endl;
-                        int sent_time_index = (int)(std::stod(sm_f.str(1).substr(0,8))*std::pow(10, 5));
+                        int sent_time_index = (int)(std::stod(sm.str(1).substr(0,8))*std::pow(10, 5));
                         if (sent_file_map.find(sent_time_index) != sent_file_map.end()){
                             std::cout << "Adding sentMessage" << std::endl;
                             float etsi_time = sent_file_map[sent_time_index];
